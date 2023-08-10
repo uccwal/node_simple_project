a9402e30acfc3e6bd54bc4a666bd1c259eec8aea
uccwal <uccwallll@gmail.com>
2023-08-10 20:47:55 +0900

    연동테스트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+export default class App extends Component {
+  state = {
+    testbody : "",
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleChange =(e)=>{
+    this.setState({
+      [e.target.name] : e.target.value,
+    });
+  }
+
+  submitId = ()=>{
+    const post ={
+      test : this.state.testbody,
+    };
+
+    fetch("http://localhost:3001/idplz", {
+      method : "post", // 통신방법
+      headers : {
+        "content-type" : "application/json",
+      },
+      body : JSON.stringify(post),
+    })
+        .then((res)=>res.json())
+        .then((json)=>{
+          this.setState({
+            testbody : json.text,
+          });
+        });
+  };
+
+  render() {
+    return (
+        <div>
+          <input onChange={this.handleChange} name ="testbody"/>
+          <button onClick = {this.submitId}>Submit</button>
+          <h1>{this.state.testbody}</h1>
+        </div>
+    )
+  }
 }
-
-export default App;
