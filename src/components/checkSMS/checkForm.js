8a4252522a52776ea75939633ec3063e4c91a349
uccwal <uccwallll@gmail.com>
2023-08-11 22:31:15 +0900

    Form data -> json으로 가공

--- src/components/checkSMS/checkForm.js
+++ src/components/checkSMS/checkForm.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, {useState} from 'react';
 import {Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField} from "@mui/material";
 import axios from 'axios';
 const CheckForm = () => {
